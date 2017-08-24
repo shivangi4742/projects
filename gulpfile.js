57aaf134f7a2f0e170789875c6a7f53db42117ff
yatishgupta <yatishg@geektree.in>
2017-08-24 12:30:06 +0530

    Login distribution being worked out.

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,51 @@ var path = require('path');
 var runSequence = require('run-sequence');
 
 const rootFolder = path.join(__dirname);
-const sharedServicesFolder = path.join(rootFolder, './sharedservices/dist')
-const sharedComponentsFolder = path.join(rootFolder, './sharedcomponents/node_modules/benowservices');
+const loginSCAssetsFolder = path.join(rootFolder, './login/src/assets');
+const sharedServicesFolder = path.join(rootFolder, './sharedservices/dist');
+const scAssetsFolder = path.join(rootFolder, './sharedcomponents/src/assets');
+const sharedComponentsFolder = path.join(rootFolder, './sharedcomponents/dist');
+const loginSSFolder = path.join(rootFolder, './login/node_modules/benowservices');
+const loginSCFolder = path.join(rootFolder, './login/node_modules/benowcomponents');
+const sharedComponentsSSFolder = path.join(rootFolder, './sharedcomponents/node_modules/benowservices');
 
 function deleteFolders(folders) {
   return del(folders);
 }
 
 gulp.task('clean:ss', function () {
-  return deleteFolders([sharedComponentsFolder]);
+  return deleteFolders([sharedComponentsSSFolder, loginSSFolder]);
+});
+
+gulp.task('clean:sc', function () {
+  return deleteFolders([loginSCFolder]);
 });
 
 gulp.task('copy:sscomponents', function () {
   return gulp.src([sharedServicesFolder + '/**/*'])
-    .pipe(gulp.dest(sharedComponentsFolder));
+    .pipe(gulp.dest(sharedComponentsSSFolder));
+});
+
+gulp.task('copy:sslogin', function () {
+  return gulp.src([sharedServicesFolder + '/**/*'])
+    .pipe(gulp.dest(loginSSFolder));
+});
+
+gulp.task('copy:scAssets', function () {
+  return gulp.src([scAssetsFolder + '/**/*'])
+    .pipe(gulp.dest(loginSCAssetsFolder));
+});
+
+gulp.task('copy:sclogin', function () {
+  return gulp.src([sharedComponentsFolder + '/**/*'])
+    .pipe(gulp.dest(loginSCFolder));
 });
 
 gulp.task('distribute:sharedservices', function () {
   runSequence(
     'clean:ss',
     'copy:sscomponents',
+    'copy:sslogin',
     function (err) {
       if (err) {
         console.log('ERROR:', err.message);
@@ -33,4 +58,17 @@ gulp.task('distribute:sharedservices', function () {
     });
 });
 
+gulp.task('distribute:sharedcomponents', function () {
+  runSequence(
+    'clean:sc',
+    'copy:sclogin',
+    'copy:scAssets',
+    function (err) {
+      if (err) {
+        console.log('ERROR:', err.message);
+      } else {
+        console.log('Compilation finished succesfully');
+      }
+    });
+});
 
