ffeda15dcb962d9fca2f61a9ba1e85467c955ea4
shivangi <shivangi47422gmail.com>
2017-10-12 15:43:09 +0530

    mglrouter

---
+++ server/server/routers/MglRouter.js
@@ -0,0 +1,22 @@
+// Library inclusions.
+var express = require('express');
+var uuid = require('uuid');
+var path = require('path');
+// Class inclusions.
+var urls = require('./../utils/URLs'); 
+var MglController = require('./../controllers/MglController');
+// Initializations.
+var router = express.Router();
+var isAuthenticated = function (req, res, next) {
+    return next();
+}
+
+var resAuthenticated = function (req, res, next) {    
+    return next();
+};
+
+router.post('/mgldetails', isAuthenticated, MglController.mglDetails.bind(MglController));
+
+router.isAuthenticated = isAuthenticated;
+router.resAuthenticated = resAuthenticated;
+module.exports = router;
