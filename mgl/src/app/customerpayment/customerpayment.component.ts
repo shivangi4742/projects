17082c648fbdd4f84233956b07c19e58358cdf22
yatishgupta <yatishg@geektree.in>
2017-11-27 20:33:32 +0530

    MGL minor changes

--- mgl/src/app/customerpayment/customerpayment.component.ts
+++ mgl/src/app/customerpayment/customerpayment.component.ts
@@ -28,7 +28,7 @@ export class CustomerpaymentComponent implements OnInit {
     //another option like collect request url
     this.payRequestService.setPayRequest(new Payrequestmodel(2,'1', false, false, true, true, 'Mahanagar Gas Limited',
       '', this.mglservice.getFailedURL(), this.mgl.name, '','', '/assets/mgl/images/0.jpg', '1', '5499', 'AACH5', '1', 'AACH5@yesbank',
-       false, false, false, true, '', this.mgl.ca, false,false,false,false, this.mglservice.getSuccessURL(), 'Benow Sales', '', '',
+       false, false, false, true, '', this.mgl.ca, false,false,false,false, this.mglservice.getSuccessURL(), 'Mahanagar Gas Limited', '', '',
      btoa(JSON.stringify(this.mgl.response)), this.mgl.ca,this.mgl.billno, '500000', '', this.supportModes))
       .then(res => this.navigate());
   }
