9f58b4bd8529974553552481d81ad8747309a198
yatishgupta <yatishg@geektree.in>
2018-01-29 12:54:31 +0530

    Hardcoded user UI for now

--- newbiz/src/app/biztopnav/biztopnav.component.ts
+++ newbiz/src/app/biztopnav/biztopnav.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from 'benowservices';
+
 @Component({
   selector: 'biztopnav',
   templateUrl: './biztopnav.component.html',
   styleUrls: ['./biztopnav.component.css']
 })
 export class BiztopnavComponent implements OnInit {
+  user: User;
 
   constructor() { }
 
   ngOnInit() {
+    this.user = new User(false, false, false, false, 0, null, '1', 'dummy@dummy.dummy', 'dummy', 'dummy', 'AL7D6', 'dummy', 'Miyaz Stores',
+      '9767843495', null, null, 'HB');
   }
 
+  goHome() {
+
+  }
 }
