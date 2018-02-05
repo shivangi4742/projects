0976c4cf47c4ac01be36c1af06b9657f9f54d0f5
yatishgupta <yatishg@geektree.in>
2018-02-05 23:11:37 +0530

    A horrible looking right menu, let's see

--- newbiz/src/app/biztopnav/biztopnav.component.ts
+++ newbiz/src/app/biztopnav/biztopnav.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
-import { User } from 'benowservices';
+import { TranslateService } from 'ng2-translate';
+
+import { User, UserService, UtilsService } from 'benowservices';
 
 @Component({
   selector: 'biztopnav',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './biztopnav.component.html',
   styleUrls: ['./biztopnav.component.css']
 })
 export class BiztopnavComponent implements OnInit {
+  @Input('language') language: number;
   @Input('user') user: User;
 
-  constructor() { }
+  constructor(private userService: UserService, private utilsService: UtilsService, private translate: TranslateService) { }
 
   ngOnInit() {
+  }
+
+  langChanged(e: any) {
+    this.language = +e;
+    this.userService.setUserLanguage(this.language);
+    this.utilsService.setLanguageInStorage(this.language);
+    this.translate.use(this.utilsService.getLanguageCode(this.language));      
+  }
+
+  signOut() {
+
   }
 }
