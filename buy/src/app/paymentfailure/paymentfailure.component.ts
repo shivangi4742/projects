0ab4d4bd77fea9e556cc877f98c2469b5cccd062
yatishgupta <yatishg@geektree.in>
2018-03-11 23:53:12 +0530

    Payment failure done, will deploy now

--- buy/src/app/paymentfailure/paymentfailure.component.ts
+++ buy/src/app/paymentfailure/paymentfailure.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { StoreService } from 'benowservices';
 
 @Component({
-  selector: 'app-paymentfailure',
+  selector: 'paymentfailure',
   templateUrl: './paymentfailure.component.html',
   styleUrls: ['./paymentfailure.component.css']
 })
 export class PaymentfailureComponent implements OnInit {
+  id: string;
+  merchant: string;
+  cartLink: string;
+  merchantCode: string;
+  settings: any;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private storeService: StoreService) { }
 
   ngOnInit() {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.merchantCode = this.activatedRoute.snapshot.params['code'];
+    this.cartLink = '/' + this.merchantCode + '/buyerinfo';
+    this.storeService.assignMerchant(this.merchantCode);
+    this.storeService.fetchStoreDetails(this.merchantCode)
+      .then(res => this.settings = res);
   }
 
 }
