79ff41f273eab6845319a4b74b65dddc2faeddf2
Yatish Gupta <yatishg@gmail.com>
2018-05-10 17:08:46 +0530

    Bugs in Add product solved

--- newbiz/src/app/addproduct/addproduct.component.ts
+++ newbiz/src/app/addproduct/addproduct.component.ts
@@ -74,6 +74,7 @@ export class AddproductComponent implements OnInit {
   price: string;
   isdisprice: boolean = false;
   idis:string;
+  errSubmit: boolean= false;
 
   constructor(private router: Router, private locationService: LocationService, private userService: UserService, private utilsService: UtilsService,
               private productService: ProductService, private fileService: FileService) {
@@ -110,6 +111,7 @@ export class AddproductComponent implements OnInit {
         tag: 'Red',
         image: '../../assets/shared/images/redCircle.png'
       }],*/
+      
       placeholder: '+Color',
       secondaryPlaceholder: 'Done',
       autocompleteOptions: {
@@ -172,7 +174,9 @@ export class AddproductComponent implements OnInit {
   }
 
   addProdColor(res: any){
+
     this.newProduct.color = res.tag;
+
   }
 
   clearProdColor(){
@@ -444,7 +448,7 @@ export class AddproductComponent implements OnInit {
           }
         }
       }
-      if(!this.newProduct.color){
+      if(!this.newProduct.color ){
         return true;
       }
     }
@@ -489,17 +493,26 @@ export class AddproductComponent implements OnInit {
     this.selectProdType('Lifestyle');
     this.newProdCheck = true;
     this.modalActions.emit({ action: "modal", params: ['close'] });
+    this.router.navigateByUrl('/catalogue');
   }
+  validateProductT
 
   onSubmit(){
+    this.errSubmit= true;
+    if(!this.checkForm()){
+      this.errSubmit= false;
     this.newProduct.prodSizes = this.prodSizes;
     this.newProduct.prodImgUrls = this.imageUrls;
     this.newProduct.variants = this.variants;
-    console.log('Submitted!', this.newProduct);
     this.productService.addProductHB(this.user.merchantCode, this.newProduct)
       .then(res => this.added(res));
 
     this.modalActions.emit({ action: "modal", params: ['open'] });
     this.newProdCheck = false;
   }
+  else {
+       this.errSubmit= true;
+ }
+
+}
 }
