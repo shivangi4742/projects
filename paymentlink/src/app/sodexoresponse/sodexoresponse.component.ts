906531aa2faccc3235188b73a05edd4531e99d00
Syn3sthete <nadarhari18@gmail.com>
2018-05-16 17:34:57 +0530

    Sodexo component commit

---
+++ paymentlink/src/app/sodexoresponse/sodexoresponse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sodexoresponse',
+  templateUrl: './sodexoresponse.component.html',
+  styleUrls: ['./sodexoresponse.component.css']
+})
+export class SodexoresponseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
